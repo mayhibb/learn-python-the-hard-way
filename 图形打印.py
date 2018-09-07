15fb7d3b910404ce98e07367b36025f1f36ded3a
mayhibb <40878861+mayhibb@users.noreply.github.com>
2018-09-07 15:46:18 +0800

    Add files via upload

---
+++ 图形打印.py
@@ -0,0 +1,213 @@
+'''
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''''
+for i in range(5):
+    for j in range(5):
+        print("* ",end="")
+    print()
+        
+print("^-^ "*7)
+
+'''
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+'''
+for i in range(5):
+    for j in range(5):
+        if i==0 or i==4 or j==0 or j ==4:
+            print("* " ,end="")
+        else:
+            print("  ",end="")
+    print()
+
+print("^-^ "*7)
+
+
+'''
+* * * * *
+* * * *
+* * *
+* *
+* 
+'''
+
+for i in range(5):
+    for j in range(5):
+        if i+j < 5 :
+            print("* ",end = "")
+        
+    print()
+    
+print("^-^ "*7)
+
+
+'''
+* * * * *
+*     *
+*   *
+* *
+*
+'''
+for i in range(5):
+    for j in range(5):
+        if i==0 or j==0 or j==(4-i):
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
+
+print("^-^ "*7)
+
+
+'''
+* 
+* *
+* * *
+* * * * 
+* * * * * 
+
+'''
+for i in range(5):
+    for j in range(i+1):
+        print("* ",end = "")
+    print()
+
+print("^-^ "*7)
+
+
+'''
+*
+* *
+*   *
+*     *
+* * * * * 
+'''
+for i in range(5):
+    for j in range(5):
+        if i == 4 or j == 0 or i == j:
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
+
+
+print("^-^ "*7)
+
+'''
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        *  
+'''
+for i in range(5):
+    for j in range(5):
+        if j>=i:
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
+
+
+print("^-^ "*7)
+
+'''
+* * * * * 
+  *     * 
+    *   * 
+      * * 
+        *
+'''
+for i in range(5):
+    for j in range(5):
+        if i == 0 or j == 4 or i ==j:
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
+
+
+print("^-^ "*7)
+
+
+'''
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * * 
+ '''
+for i in range(6):
+    for k in range(5-i):
+        print(" ",end="")
+    for j in range(i+1):
+        print("* ",end="")
+    print()
+       
+print("^-^ "*7)           
+'''
+     *
+    * *
+   *   *
+  *     *
+ *       *
+* * * * * *
+'''
+
+
+for i in range(6):
+    for j in range(5-i):
+        print(" ",end="")
+    for k in range(i+1):
+        if i ==5 or k==0 or k==i:
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
+        
+print("^-^ "*7)   
+    
+        
+
+
+'''
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+*                 *
+* *             * *
+* * *         * * * 
+* * * *     * * * * 
+* * * * * * * * * *
+
+'''
+for i in range(10):
+    for j in range(10):
+        if i < 5:
+            if  j < (10-i*2)/2 or j>= (10-(10-i*2)/2):
+                print("* ",end="")
+            else:
+                print("  ",end="")
+        else:
+            if j < ((i*2 -10)/2+1) or j>= (10-((i*2 -10)/2+1)):
+                print("* ",end = "")
+            else:
+                print("  ",end="")
+        
+    print()
+
+    
+print("^-^ "*7)
+
+
+
